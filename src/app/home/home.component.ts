ec5d2de8318a64942f1a2e7ad3ba0945c16d3b9f
Liam Robertson <liam.robertson@fdmgroup.com>
2021-09-09 23:42:40 +0100

    cleaned up a bit, still works but tests are broken now

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,7 +25,6 @@ export class HomeComponent implements OnInit {
 	public poemListSorted: number[][];
 	public startingPoem: number[];
 	public poemListPath: string;
-	yourVariable: any;
 	numOfPoems: any;
 	poemOrder: any;
 	finalPoemList: string[];
@@ -38,6 +37,33 @@ export class HomeComponent implements OnInit {
 	outputPath: string;
 	startingPoemRaw: any;
 	loopCounter: any;
+	uniqueCoordList1: any;
+	uniqueCoordList2: any;
+	uniqueCoordList3: any;
+	nextAxisNumberDict: { 0: number; 1: number; 2: number; };
+	outputList: any[];
+	successCounter: number;
+	currentAxisNumber: number;
+	xLoopCounter: number;
+	yLoopCounter: number;
+	zLoopCounter: number;
+	currentTargetPoem: number[];
+	axisDict: { 0: any; 1: any; 2: any; };
+	currentXUniqueList: any;
+	currentYUniqueList: any;
+	currentZUniqueList: any;
+	currentXCoord: any;
+	currentYCoord: any;
+	currentZCoord: any;
+	targetPoemDict: { 0: any[]; 1: any[]; 2: any[]; };
+	uniqueCoordList1Raw: number[];
+	uniqueCoordList2Raw: number[];
+	uniqueCoordList3Raw: number[];
+	xIndex: number;
+	yIndex: number;
+	zIndex: number;
+	currentTemplatePoem: string;
+	outputListRaw: any[];
 
 	constructor(private router: Router, private electronService: ElectronService, private fb: FormBuilder) {  
 	}
@@ -68,90 +94,90 @@ export class HomeComponent implements OnInit {
 		this.numOfPoems = this.poemFormGroup.value.numOfPoemsControl
 		this.poemOrder = this.poemFormGroup.value.poemOrderControl
 
-		this.finalPoemList = this.iterateBySyllables(this.poemListSorted, this.startingPoem, this.numOfPoems, this.poemOrder)
+		this.finalPoemList = this.iterateBySyllables(this.poemList, this.startingPoem, this.numOfPoems, this.poemOrder)
 		console.log(this.finalPoemList);
-		this.templateList = await this.createTemplateList(this.finalPoemList)
-		this.writeDocument(this.templateList)
+		this.templateList = await this.createTemplateList()
+		this.writeDocument()
 		await this.sleep(1500);
 
 		this.loadingBool = false;
 		this.successBool = true;
 	}
 
-	public iterateBySyllables(poemList: number[][], startingPoem: number[], numOfPoems: number, poemOrder: string) {
-		let uniqueCoordList1 = this.updateUniqueLists(poemList, startingPoem)[0]
-		let uniqueCoordList2 = this.updateUniqueLists(poemList, startingPoem)[1]
-		let uniqueCoordList3 = this.updateUniqueLists(poemList, startingPoem)[2]
+	public iterateBySyllables(poemList, startingPoem, numOfPoems, poemOrder) {
+		this.nextAxisNumberDict = {0: 1, 1: 2, 2: 0};
+		this.outputList = []
+		this.successCounter = 0
+		this.currentAxisNumber = 0
+		this.xLoopCounter = 0
+		this.yLoopCounter = 0
+		this.zLoopCounter = 0
+		this.currentTargetPoem = this.startingPoem
 
-		let nextAxisNumberDict = {0:1, 1:2, 2:0};
-		let outputList = []
-		let successCounter: number = 0
-		let currentAxisNumber = 0
-		let xLoopCounter = 0
-		let yLoopCounter = 0
-		let zLoopCounter = 0
-		let currentTargetPoem = startingPoem
+		this.uniqueCoordList1 = this.updateUniqueLists()[0]
+		this.uniqueCoordList2 = this.updateUniqueLists()[1]
+		this.uniqueCoordList3 = this.updateUniqueLists()[2]
 
-		while (successCounter < numOfPoems) {
-			let axisDict = {0:uniqueCoordList1, 1:uniqueCoordList2, 2:uniqueCoordList3}
-			let currentXUniqueList = axisDict[currentAxisNumber]
-			let currentYUniqueList = axisDict[nextAxisNumberDict[currentAxisNumber]]
-			let currentZUniqueList = axisDict[nextAxisNumberDict[nextAxisNumberDict[currentAxisNumber]]]
-			let currentXCoord = currentXUniqueList[xLoopCounter]
-			let currentYCoord = currentYUniqueList[yLoopCounter]
-			let currentZCoord = currentZUniqueList[zLoopCounter]
-			let targetPoemDict = {0: [currentXCoord, currentYCoord, currentZCoord], 1: [currentZCoord, currentXCoord, currentYCoord], 2: [currentYCoord, currentZCoord, currentXCoord]}
-			let currentTargetPoem = targetPoemDict[currentAxisNumber]
+		while (this.successCounter < this.numOfPoems) {
+			this.axisDict = {0: this.uniqueCoordList1, 1: this.uniqueCoordList2, 2: this.uniqueCoordList3}
+			this.currentXUniqueList = this.axisDict[this.currentAxisNumber]
+			this.currentYUniqueList = this.axisDict[this.nextAxisNumberDict[this.currentAxisNumber]]
+			this.currentZUniqueList = this.axisDict[this.nextAxisNumberDict[this.nextAxisNumberDict[this.currentAxisNumber]]]
+			this.currentXCoord = this.currentXUniqueList[this.xLoopCounter]
+			this.currentYCoord = this.currentYUniqueList[this.yLoopCounter]
+			this.currentZCoord = this.currentZUniqueList[this.zLoopCounter]
+			this.targetPoemDict = {0: [this.currentXCoord, this.currentYCoord, this.currentZCoord], 1: [this.currentZCoord, this.currentXCoord, this.currentYCoord], 2: [this.currentYCoord, this.currentZCoord, this.currentXCoord]}
+			this.currentTargetPoem = this.targetPoemDict[this.currentAxisNumber]
 			
-			if (!this.checkArrIn2dMatrix(outputList, currentTargetPoem) && this.checkArrIn2dMatrix(poemList, currentTargetPoem)) {
-				console.log(successCounter);
-				uniqueCoordList1 = this.updateUniqueLists(poemList, currentTargetPoem)[0]
-				uniqueCoordList2 = this.updateUniqueLists(poemList, currentTargetPoem)[1]
-				uniqueCoordList3 = this.updateUniqueLists(poemList, currentTargetPoem)[2]
+			if (!this.checkArrIn2dMatrix(this.outputList, this.currentTargetPoem) && this.checkArrIn2dMatrix(this.poemList, this.currentTargetPoem)) {
+				console.log(this.successCounter);
+				this.uniqueCoordList1 = this.updateUniqueLists()[0]
+				this.uniqueCoordList2 = this.updateUniqueLists()[1]
+				this.uniqueCoordList3 = this.updateUniqueLists()[2]
 				
-				currentAxisNumber = nextAxisNumberDict[currentAxisNumber]
-				outputList.push(currentTargetPoem.slice(0));
-				poemList = this.removeItem(poemList, currentTargetPoem);
+				this.currentAxisNumber = this.nextAxisNumberDict[this.currentAxisNumber]
+				this.outputList.push(this.currentTargetPoem.slice(0));
+				this.poemList = this.removeItem(this.poemList, this.currentTargetPoem);
 
-				zLoopCounter  = 0;
-				yLoopCounter = 0;
-				xLoopCounter = 0;
-				successCounter++
-			} else if (currentXCoord == currentXUniqueList[currentXUniqueList.length - 1]) {
-				if (currentYCoord == currentYUniqueList[currentYUniqueList.length - 1]) {
-					if (currentZCoord == currentZUniqueList[currentZUniqueList.length - 1]) {
-						zLoopCounter  = 0;
-						yLoopCounter = 0;
-						xLoopCounter = 0;
+				this.zLoopCounter  = 0;
+				this.yLoopCounter = 0;
+				this.xLoopCounter = 0;
+				this.successCounter++
+			} else if (this.currentXCoord == this.currentXUniqueList[this.currentXUniqueList.length - 1]) {
+				if (this.currentYCoord == this.currentYUniqueList[this.currentYUniqueList.length - 1]) {
+					if (this.currentZCoord == this.currentZUniqueList[this.currentZUniqueList.length - 1]) {
+						this.zLoopCounter  = 0;
+						this.yLoopCounter = 0;
+						this.xLoopCounter = 0;
 					}
-					zLoopCounter += 1;
-					yLoopCounter = 0;
-					xLoopCounter = 0;
+					this.zLoopCounter += 1;
+					this.yLoopCounter = 0;
+					this.xLoopCounter = 0;
 				}
-				yLoopCounter += 1;
-				xLoopCounter = 0;
+				this.yLoopCounter += 1;
+				this.xLoopCounter = 0;
 			} else {
-				xLoopCounter += 1;
+				this.xLoopCounter += 1;
 			}
 		}
-		if (poemOrder == "end") {
-			outputList = outputList.reverse();
+		if (this.poemOrder == "end") {
+			this.outputList = this.outputList.reverse();
 		}
-		const finalOutputList = outputList.map(value => value.join("-").toString())		
+		const finalOutputList = this.outputList.map(value => value.join("-").toString())		
 		return finalOutputList
 	}
 
-	public updateUniqueLists(poemList: number[][], currentPoem: number[]) {
-		const uniqueCoordList1Raw: number[] = this.sortedUniqueList(poemList, 0)
-		const uniqueCoordList2Raw: number[] = this.sortedUniqueList(poemList, 1)
-		const uniqueCoordList3Raw: number[] = this.sortedUniqueList(poemList, 2)
-		const xIndex = uniqueCoordList1Raw.indexOf(currentPoem[0])
-		const yIndex = uniqueCoordList2Raw.indexOf(currentPoem[1])
-		const zIndex = uniqueCoordList3Raw.indexOf(currentPoem[2])
-		const uniqueCoordList1: number[] = uniqueCoordList1Raw.slice(xIndex).concat(uniqueCoordList1Raw.slice(0, xIndex));
-		const uniqueCoordList2: number[] = uniqueCoordList2Raw.slice(yIndex).concat(uniqueCoordList2Raw.slice(0, yIndex));
-		const uniqueCoordList3: number[] = uniqueCoordList3Raw.slice(zIndex).concat(uniqueCoordList3Raw.slice(0, zIndex));
-		return [uniqueCoordList1, uniqueCoordList2, uniqueCoordList3]
+	public updateUniqueLists() {
+		this.uniqueCoordList1Raw = this.sortedUniqueList(this.poemList, 0)
+		this.uniqueCoordList2Raw = this.sortedUniqueList(this.poemList, 1)
+		this.uniqueCoordList3Raw = this.sortedUniqueList(this.poemList, 2)
+		this.xIndex = this.uniqueCoordList1Raw.indexOf(this.currentTargetPoem[0])
+		this.yIndex = this.uniqueCoordList2Raw.indexOf(this.currentTargetPoem[1])
+		this.zIndex = this.uniqueCoordList3Raw.indexOf(this.currentTargetPoem[2])
+		this.uniqueCoordList1 = this.uniqueCoordList1Raw.slice(this.xIndex).concat(this.uniqueCoordList1Raw.slice(0, this.xIndex));
+		this.uniqueCoordList2 = this.uniqueCoordList2Raw.slice(this.yIndex).concat(this.uniqueCoordList2Raw.slice(0, this.yIndex));
+		this.uniqueCoordList3 = this.uniqueCoordList3Raw.slice(this.zIndex).concat(this.uniqueCoordList3Raw.slice(0, this.zIndex));
+		return [this.uniqueCoordList1, this.uniqueCoordList2, this.uniqueCoordList3]
 	}
 
 	public checkArrIn2dMatrix(matrix, testArr) {
@@ -163,14 +189,14 @@ export class HomeComponent implements OnInit {
 		return arr.filter(f => f.toString() !== item.toString())
 	}
 
-	public async createTemplateList(poemList: string[]) {
-		const outputList = [];
-		for (let index = 0; index < poemList.length; index++) {		
+	public async createTemplateList() {
+		this.outputList = [];
+		for (let index = 0; index < this.finalPoemList.length; index++) {		
 
-			const currentTargetPoem = poemList[index]
+			this.currentTemplatePoem = this.finalPoemList[index]
 			let hasTextVar = true;
-			const currentPoemName = poemList[index] + '.json'
-			const currentGlyphName = poemList[index] + '.jpg'
+			const currentPoemName = this.finalPoemList[index] + '.json'
+			const currentGlyphName = this.finalPoemList[index] + '.jpg'
 			const poemPath = join(resolve(__dirname, "../../../../../../src/assets/syllabary-poems"), currentPoemName);		
 			const glyphPath = join(resolve(__dirname, "../../../../../../src/assets/syllabary-glyphs-jpg"), currentGlyphName); 
 			const poemGlyph = await this.electronService.readFile(glyphPath, {})	
@@ -183,20 +209,20 @@ export class HomeComponent implements OnInit {
 			if (contentJson['text'] === null) {
 					hasTextVar = false;
 			}
-			outputList.push({
-			"code": currentTargetPoem,
+			this.outputList.push({
+			"code": this.currentTemplatePoem,
 			"title": contentJson['title'],
 			"text": contentJson['text'],
 			"glyph": poemGlyph,
 			"hasText": hasTextVar,
 	})}		
-		return outputList
+		return this.outputList
 	}
 
-	public writeDocument(outputList) {
+	public writeDocument() {
 		const docContentList = [];
-		for (let index = 0; index < outputList.length; index++) { 
-			const poemGlyph = outputList[index]["glyph"]			
+		for (let index = 0; index < this.outputList.length; index++) { 
+			const poemGlyph = this.outputList[index]["glyph"]			
 			const poemImage = new ImageRun({
 				data: poemGlyph,
 				transformation: {
@@ -218,8 +244,8 @@ export class HomeComponent implements OnInit {
 			let currentTitle = 
 				new Paragraph({
 					children: [ 
-						new TextRun({text: outputList[index]["title"], font: "Underwood Champion", size: 40}),
-						new TextRun({text: " (" + outputList[index]["code"] + ")", font: "Underwood Champion", size: 30}),
+						new TextRun({text: this.outputList[index]["title"], font: "Underwood Champion", size: 40}),
+						new TextRun({text: " (" + this.outputList[index]["code"] + ")", font: "Underwood Champion", size: 30}),
 					],
 					pageBreakBefore: true,
 					alignment: AlignmentType.CENTER,
@@ -228,7 +254,7 @@ export class HomeComponent implements OnInit {
 			let currentText = 
 				new Paragraph({
 					children: [ 
-						new TextRun({text: outputList[index]["text"], font: "Underwood Champion", size: 30, break: 2}),
+						new TextRun({text: this.outputList[index]["text"], font: "Underwood Champion", size: 30, break: 2}),
 					],
 				})
 
@@ -256,24 +282,24 @@ export class HomeComponent implements OnInit {
 	}
 
 	public sortByMultipleValues(inputList: number[][]) {
-		const outputListRaw = [];
+		this.outputListRaw = [];
 		const tempList = [];
-		const uniqueCoordList1 = this.sortedUniqueList(inputList, 0)
-		const uniqueCoordList2 = this.sortedUniqueList(inputList, 1)
-		const uniqueCoordList3 = this.sortedUniqueList(inputList, 2)
+		this.uniqueCoordList1 = this.sortedUniqueList(inputList, 0)
+		this.uniqueCoordList2 = this.sortedUniqueList(inputList, 1)
+		this.uniqueCoordList3 = this.sortedUniqueList(inputList, 2)
 
-		for (let xj=0; xj < uniqueCoordList1.length; xj++) {
-				for (let yj=0; yj < uniqueCoordList2.length; yj++) {
-						for (let zj=0; zj < uniqueCoordList3.length; zj++) {
+		for (let xj=0; xj < this.uniqueCoordList1.length; xj++) {
+				for (let yj=0; yj < this.uniqueCoordList2.length; yj++) {
+						for (let zj=0; zj < this.uniqueCoordList3.length; zj++) {
 								for (let i=0; i < inputList.length; i++) {
-										if (inputList[i][0] == uniqueCoordList1[xj] && inputList[i][1] == uniqueCoordList2[yj] && inputList[i][2] == uniqueCoordList3[zj]) {
+										if (inputList[i][0] == this.uniqueCoordList1[xj] && inputList[i][1] == this.uniqueCoordList2[yj] && inputList[i][2] == this.uniqueCoordList3[zj]) {
 												tempList.push(inputList[i]);
 										}
 								}
 						}
 				}
 		}
-		const outputList = outputListRaw.concat(tempList);
+		const outputList = this.outputListRaw.concat(tempList);
 	return outputList
 	}
 
